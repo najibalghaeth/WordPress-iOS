797aa34bb406ccb32a120f311e2ae6c3ad806322
Jorge Leandro Perez <jorge@lantean.co>
2018-02-19 14:13:56 -0300

    New WordPressAuth.framework project

---
+++ WordPressAuth/WordPressAuth/WordPressAuth.h
@@ -0,0 +1,11 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for WordPressAuth.
+FOUNDATION_EXPORT double WordPressAuthVersionNumber;
+
+//! Project version string for WordPressAuth.
+FOUNDATION_EXPORT const unsigned char WordPressAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WordPressAuth/PublicHeader.h>
+
+
