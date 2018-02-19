0597fbd59d35bd9ad46082e751d738ffdbd889c4
Jorge Leandro Perez <jorge@lantean.co>
2018-02-19 18:02:22 -0300

    WordPressAuth: Updates Public Header

--- WordPressAuth/WordPressAuth/WordPressAuth.h
+++ WordPressAuth/WordPressAuth/WordPressAuth.h
@@ -9,3 +9,7 @@ FOUNDATION_EXPORT const unsigned char WordPressAuthVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <WordPressAuth/PublicHeader.h>
 
 
+#import <WordPressAuth/WPNUXPrimaryButton.h>
+#import <WordPressAuth/WPNUXSecondaryButton.h>
+#import <WordPressAuth/WPNUXMainButton.h>
+#import <WordPressAuth/WPWalkthroughTextField.h>
